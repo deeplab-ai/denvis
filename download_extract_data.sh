21da9a2d53adb4e8aa928ef58e13d2fbf74a01bf
Agamemnon <a.krasoulis@deeplab.ai>
2022-02-08 14:09:45 +0200

    final

--- download_extract_data.sh
+++ download_extract_data.sh
@@ -6,8 +6,8 @@ wget -P data/outputs http://bits.csb.pitt.edu/files/rfnn_dude_scores.tgz
 wget -P data/outputs https://storage.googleapis.com/denvis_v1_outputs/docking_performance_scores.tar.gz
 
 # Extract data
+cd data/outputs
 for file in *.tar.gz; do tar xzvf "${file}" && rm "${file}"; done
 
 # Extract RF/NN-score .tgz file into new dir and remove
-mkdir rfnn_dude_scores && tar zxvf rfnn_dude_scores.tgz -d rfnn_dude_scores/
-rm rfnn_dude_scores.tgz
+mkdir rfnn_dude_scores && tar zxvf rfnn_dude_scores.tgz -C rfnn_dude_scores/ && rm rfnn_dude_scores.tgz
