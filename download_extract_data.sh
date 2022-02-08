b0e80d2661e24ca8f68c88fa882bb22ae9f74254
Agamemnon <a.krasoulis@deeplab.ai>
2022-02-08 13:54:42 +0200

    update

--- download_extract_data.sh
+++ download_extract_data.sh
@@ -2,12 +2,12 @@
 wget -P data/outputs/ https://storage.googleapis.com/denvis_v1_outputs/denvis_outputs.tar.gz
 wget -P data/outputs https://storage.googleapis.com/denvis_v1_outputs/deepdta_outputs.tar.gz
 wget -P data/outputs https://storage.googleapis.com/denvis_v1_outputs/vina_outputs.tar.gz
-wget -P http://bits.csb.pitt.edu/files/rfnn_dude_scores.tgz
-wget -P https://storage.googleapis.com/denvis_v1_outputs/docking_performance_scores.tar.gz
+wget -P data/outputs http://bits.csb.pitt.edu/files/rfnn_dude_scores.tgz
+wget -P data/outputs https://storage.googleapis.com/denvis_v1_outputs/docking_performance_scores.tar.gz
 
 # Extract data
 for file in *.tar.gz; do tar xzvf "${file}" && rm "${file}"; done
 
-# Extract RF/NN-score .tgz file into new dir
+# Extract RF/NN-score .tgz file into new dir and remove
 mkdir rfnn_dude_scores && tar zxvf rfnn_dude_scores.tgz -d rfnn_dude_scores/
-
+rm rfnn_dude_scores.tgz
