6b7124c1395e2f150b14ec8c980ffbd852f32cb8
nickantonopoulos <nanton96@bitbucket.org>
2022-03-11 10:59:44 +0200

    [5] added some comments

--- scripts/drop_sdf_duplicates.py
+++ scripts/drop_sdf_duplicates.py
@@ -12,11 +12,10 @@ It keeps the last entry in the file.
 @click.argument('sdf_file', type=str)
 @click.option('--output_file', '-o', type=str, default='ligands_dedup.sdf')
 def drop_sdf_duplicates(sdf_file, output_file):
-    # Read sdf file
-
     assert sdf_file.endswith(".sdf")
     assert output_file.endswith(".sdf")
 
+    # Read sdf file
     with open(sdf_file, "r") as f:
         contents = f.readlines()
 
@@ -26,14 +25,17 @@ def drop_sdf_duplicates(sdf_file, output_file):
     start_of_mol = 0
     end_of_mol = None
     for i, line in enumerate(contents):
+        # mols are separated with $$$$ in the sdf file
         if line == "$$$$\n":
             end_of_mol = i
-
             ids.append(contents[start_of_mol])
+            # joins all the lines that correspond to a single molecule
+            # and saves them in the mols list
             mols.append("".join(contents[start_of_mol:end_of_mol + 1]))
             # re init for next
             start_of_mol = i + 1
 
+    # save the ids into a DataFrame for easy deduplication
     id_df = pd.DataFrame({"ligand_id": ids})
 
     # drop duplicates, keeping last
